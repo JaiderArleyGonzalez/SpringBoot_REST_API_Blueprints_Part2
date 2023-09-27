0af5f29e3aa7d8f7fead81c4d4782a6b26276699
Jaider Gonzalez <jaider.gonzalez@mail.escuelaing.edu.co>
2023-09-27 08:37:10 -0500

    mitad de la primera parte

--- src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintAPIController.java
+++ src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintAPIController.java
@@ -8,19 +8,39 @@ package edu.eci.arsw.blueprints.controllers;
 import java.util.LinkedHashSet;
 import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import edu.eci.arsw.blueprints.persistence.BlueprintNotFoundException;
+import edu.eci.arsw.blueprints.services.BlueprintsServices;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author hcadavid
  */
+@RestController
+@RequestMapping(value = "/blueprints")
 public class BlueprintAPIController {
-    
-    
-    
-    
-    
+    @Autowired
+    BlueprintsServices bps;
+
+    @RequestMapping(method = RequestMethod.GET)
+        public ResponseEntity<?> manejadorGetRecursoBlueprints(){
+            try {
+                //obtener datos que se enviarán a través del API
+                return new ResponseEntity<>(bps.getAllBlueprints(), HttpStatus.ACCEPTED);
+            } catch (BlueprintNotFoundException  ex) {
+                Logger.getLogger(BlueprintAPIController.class.getName()).log(Level.SEVERE, null, ex);
+                return new ResponseEntity<>(ex.getMessage(),HttpStatus.NOT_FOUND);
+            }
+            
+            
+            
+        }
 }
 
