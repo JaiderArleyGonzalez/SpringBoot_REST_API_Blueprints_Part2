b1cc15ce4a74e7578efb79589fe45a39059c49a6
Jaider Gonzalez <jaider.gonzalez@mail.escuelaing.edu.co>
2023-09-27 14:16:59 -0500

    Punto I terminado

--- src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintAPIController.java
+++ src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintAPIController.java
@@ -10,6 +10,8 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,18 +31,35 @@ public class BlueprintAPIController {
     @Autowired
     BlueprintsServices bps;
 
-    @RequestMapping(method = RequestMethod.GET)
-        public ResponseEntity<?> manejadorGetRecursoBlueprints(){
-            try {
-                //obtener datos que se enviarán a través del API
-                return new ResponseEntity<>(bps.getAllBlueprints(), HttpStatus.ACCEPTED);
-            } catch (BlueprintNotFoundException  ex) {
-                Logger.getLogger(BlueprintAPIController.class.getName()).log(Level.SEVERE, null, ex);
-                return new ResponseEntity<>(ex.getMessage(),HttpStatus.NOT_FOUND);
-            }
-            
-            
-            
+    @GetMapping(produces = "application/json")
+    public ResponseEntity<?> manejadorGetRecursoBlueprints(){
+        try {
+            //obtener datos que se enviarán a través del API
+            return new ResponseEntity<>(bps.getAllBlueprints(), HttpStatus.ACCEPTED);
+        } catch (BlueprintNotFoundException  ex) {
+            Logger.getLogger(BlueprintAPIController.class.getName()).log(Level.SEVERE, null, ex);
+            return new ResponseEntity<>(ex.getMessage(),HttpStatus.NOT_FOUND);
         }
+                    
+    }
+
+    @GetMapping(produces = "application/json", path="/{author}")
+    public ResponseEntity<?> manejadorGetRecursoBlueprintsByAuthor(@PathVariable("author") String author) {
+        try {
+            return new ResponseEntity<>(bps.getBlueprintsByAuthor(author), HttpStatus.ACCEPTED);
+        } catch (BlueprintNotFoundException e) {
+            Logger.getLogger(BlueprintAPIController.class.getName()).log(Level.SEVERE, null, e);
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
+    }
+    @GetMapping(produces = "application/json", path="/{author}/{bpname}")
+    public ResponseEntity<?> manejadorGetRecursoBlueprintsByAuthor(@PathVariable("author") String author, @PathVariable("bpname") String bpname) {  
+        try {
+            return new ResponseEntity<>(bps.getBlueprint(author, bpname), HttpStatus.ACCEPTED);
+        } catch (BlueprintNotFoundException e) {
+            Logger.getLogger(BlueprintAPIController.class.getName()).log(Level.SEVERE, null, e);
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
+    }
 }
 
--- src/main/java/edu/eci/arsw/blueprints/persistence/impl/InMemoryBlueprintPersistence.java
+++ src/main/java/edu/eci/arsw/blueprints/persistence/impl/InMemoryBlueprintPersistence.java
@@ -33,14 +33,15 @@ public class InMemoryBlueprintPersistence implements BlueprintsPersistence{
         //load stub data
         
         Point[] pts1=new Point[]{new Point(140, 140),new Point(115, 115)};
-        Blueprint bp1=new Blueprint("Jaider Gonzalez", "Casa Vacacional",pts1);
+        Blueprint bp1=new Blueprint("juan", "casa",pts1);
         blueprints.put(new Tuple<>(bp1.getAuthor(),bp1.getName()), bp1);
         Point[] pts2=new Point[]{new Point(140, 140),new Point(115, 115)};
-        Blueprint bp2=new Blueprint("Jaider Gonzalez", "Casa de invierno",pts2);
+        Blueprint bp2=new Blueprint("juan", "Casa de invierno",pts2);
         blueprints.put(new Tuple<>(bp2.getAuthor(),bp2.getName()), bp2);
         Point[] pts3=new Point[]{new Point(140, 140),new Point(115, 115)};
         Blueprint bp3=new Blueprint("Miguel Barrera", "Bloque F ",pts3);
         blueprints.put(new Tuple<>(bp3.getAuthor(),bp3.getName()), bp3);
+        
     }    
     
     @Override
@@ -62,7 +63,7 @@ public class InMemoryBlueprintPersistence implements BlueprintsPersistence{
     public Set<Blueprint> getBlueprintsByAuthor(String author) throws BlueprintNotFoundException {
         Set<Blueprint> result = new HashSet<>();
         for (Blueprint value : blueprints.values()) { 
-            if (value.getAuthor() == author) {
+            if (value.getAuthor().equals(author)) {
                 result.add(value);
             }
         }
